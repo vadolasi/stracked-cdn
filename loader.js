717df3b1c11c64a3011889b9889525bcdae74109
Douglas de Araújo <douglas@3xweb.site>
2024-09-02 20:10:14 -0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,20 +1,32 @@
 (function () {
-  const websiteUuid = window.abTestConfig && window.abTestConfig.websiteUuid;
-  if (!websiteUuid) {
-    console.error("UUID is not provided in the abTestConfig configuration");
+  // Function to get query parameter from the script's URL
+  function getQueryParam(param) {
+    const scripts = document.getElementsByTagName("script");
+    const script = scripts[scripts.length - 1]; // This script is the last loaded script
+    const queryString = script.src.split("?")[1] || "";
+    const params = new URLSearchParams(queryString);
+    return params.get(param);
+  }
+
+  const strackedId = getQueryParam("stracked");
+
+  if (!strackedId) {
+    console.error("UUID is not provided");
     return;
   }
 
   const apiUrl = "https://stracked-front.vercel.app/api/check-ab-tests"; // Endpoint da API para verificar o UUID
 
-  async function checkForABTests(apiUrl, websiteUuid) {
+  async function checkForABTests(apiUrl, strackedId) {
     try {
-      const response = await fetch(`${apiUrl}?id=${websiteUuid}`);
+      const response = await fetch(`${apiUrl}?id=${strackedId}`);
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       const data = await response.json();
-      return data.hasTests ? data.tests : null;
+
+      console.log(data);
+      return data ? data : null;
     } catch (error) {
       console.error("Error checking for AB tests:", error);
       return null;
@@ -22,18 +34,23 @@
   }
 
   function runABTests(tests) {
-    const currentUrl = window.location.href; // Usar href para incluir o domínio completo na comparação
+    const currentUrl = new URL(window.location.href); // Usar URL para facilitar a manipulação de parâmetros
+    const searchParams = currentUrl.search; // Obter os parâmetros da URL atual
+    
     tests.forEach((test) => {
-      if (currentUrl === test.entryUrl) {
+      if (currentUrl.href.startsWith(test.entryUrl)) { // Usar startsWith para maior flexibilidade
         const randomIndex = Math.floor(Math.random() * test.variants.length);
-        const targetUrl = test.variants[randomIndex].pageUrl;
-        window.location.href = targetUrl;
+        let targetUrl = new URL(test.variants[randomIndex]); // Criar URL para manipular
+        targetUrl.search = searchParams; // Atribuir os parâmetros da URL atual à URL de destino
+        
+        window.location.href = targetUrl.href; // Redirecionar para a URL de destino com parâmetros
       }
     });
   }
+  
 
   (async function initABTests() {
-    const tests = await checkForABTests(apiUrl, websiteUuid);
+    const tests = await checkForABTests(apiUrl, strackedId);
     if (tests) {
       runABTests(tests);
     }
