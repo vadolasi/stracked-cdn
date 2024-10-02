d29a34a9f64bc1985e3396d9b9a39e3e13f896bc
Vitor Daniel <vitor036daniel@gmail.com>
2024-10-01 23:24:52 -0300

    Adicionar terser para melhorar minificação
    O arquivo foi reduzido de 176Kb para 126Kb

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 import esbuild from "esbuild";
+import { minify } from "terser";
 import { writeFile } from "fs/promises";
 
 const checkABTestsAndTrackingUrl = process.env.CHECK_AB_TESTS_AND_TRACKING_URL ?? "https://app.stracked.com.br/api/check-ab-test-and-tracking";
@@ -14,39 +15,35 @@ async function build() {
     format: "esm",
     platform: "browser",
     write: false,
+    mangleQuoted: true,
+    pure: ["Error", "console"],
     define: {
       "process.env.CHECK_AB_TESTS_AND_TRACKING_URL": `"${checkABTestsAndTrackingUrl}"`,
       "process.env.TRACKING_URL": `"${trackingUrl}"`,
     },
   });
 
-  /*
   const minified = await minify(result.outputFiles[0].text, {
     mangle: {
       properties: {
         builtins: true,
+        reserved: ["constructor"],
       },
     },
     compress: {
-      passes: 4,
-      pure_funcs: ["Error"],
+      passes: 3,
+      pure_funcs: ["Error", "console"],
       booleans_as_integers: true,
       arguments: true,
       drop_console: true,
       hoist_funs: true,
-      global_defs: {
-        "@globalThis": "window",
-      },
     },
-    ecma: 2022,
+    ecma: 2020,
     toplevel: true,
     module: true,
   });
 
   await writeFile("loader.js", minified.code);
-  */
-
-  await writeFile("loader.js", result.outputFiles[0].text);
 }
 
 build();
